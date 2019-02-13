bddd5df4fbee592e0dfbe6d2b010c8b757eceb64
OJP98 <ojuarez_p@hotmail.com>
2019-02-13 02:21:16 -0600

    Adición de la página web

---
+++ aerolineas/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aerolineas.Models;
+
+namespace aerolineas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+
+        public IActionResult Analisis()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
